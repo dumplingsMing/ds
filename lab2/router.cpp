4bfd75d8884666e37d7f58ec5e8c7c1fdb2516a4
dumplings_ming <dumplings_ming@126.com>
2016-03-24 22:15:45 +0800

    created lab2

---
+++ lab2/router.cpp
@@ -0,0 +1,51 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+
+static int
+open_receivefd(int port)
+{
+    /* create a fd for server */
+    int receivefd;
+    if (receivefd = socket(AF_INET, SOCK_STREAM, 0) < 0)
+        return -1;
+
+    /* set SO_REUSERADDR to true for immiderate restart sercver */
+    int optval = 1
+    if (setsockopt(receivefd, SOL_SOCKET, SO_REUSEADDR, 
+		   (const void *)&optval , sizeof(int)) < 0)
+	    return -1;
+
+    /* create a socket address and bind it to listen fd */
+    struct sockaddr_in serveraddr;
+    bzero((char *)&serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons((unsigned short)port);
+
+    /* bind the address to receive fd */
+    if (bind(reveivefd, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0)
+        return -1;
+    return receivefd;
+}
+
+void 
+start_receiver(int port)
+{
+    int recv_fd;
+    if ((recv_fd = open_receivefd(port)) < 0) {
+        cerr << "error when opening receive fd";
+        return -1;
+    }
+
+    int recv_len;
+    struct sockaddr_in senderaddr;
+    char buf[MAX_BUF];
+    for (;;) {
+        if ((recv_len = recvform(recv_fd, buf, MAXBUF, 0, NULL, NULL)) < 0) {
+            cerr << "error when receiving data" << endl;
+            return -1;
+        }
+    }
+    close(rcvfd);
+    return 0;
+}
---
+++ lab2/router.h
@@ -0,0 +1,6 @@
+#include <string>
+
+#define MAX_BUF 4096
+
+void start_receiver(int port);
+void start_sender(std::string filename);
